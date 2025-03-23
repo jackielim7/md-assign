a2255100fa7ace989e0327ea2dc3e36b2e677d7e
jackielim7 <jackieops78@gmail.com>
2025-03-24 03:31:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,32 +23,31 @@ def main():
       st.write("This is a raw data")
       st.dataframe(df)
 
-    st.subheader("📈 Data Visualization")
   with st.expander("Click to expand/minimize"):
-    st.write("### Scatter Plot of Height vs. Weight by Obesity Level")
-        # Create scatter plot
-    fig, ax = plt.subplots(figsize=(8, 5))
-    scatter = sns.scatterplot(
-      data=df, 
-      x="Height", 
-      y="Weight", 
-      hue="NObeyesdad", 
-      palette="Set2", 
-      s=100, 
-      edgecolor="black"
-    )
-    # Improve legend placement
-    legend = plt.legend(
-      title="NObeyesdad",
-      bbox_to_anchor=(1.05, 1), 
-      loc='upper left', 
-      borderaxespad=0
-    )
-        
-    # Set labels & title
-    plt.xlabel("Height")
-    plt.ylabel("Weight")
-    plt.title("Data Visualization", fontsize=12, fontweight="bold")
+      st.write("### Scatter Plot of Height vs. Weight by Obesity Level")
+          # Create scatter plot
+      fig, ax = plt.subplots(figsize=(8, 5))
+      scatter = sns.scatterplot(
+        data=df, 
+        x="Height", 
+        y="Weight", 
+        hue="NObeyesdad", 
+        palette="Set2", 
+        s=100, 
+        edgecolor="black"
+      )
+      # Improve legend placement
+      legend = plt.legend(
+        title="NObeyesdad",
+        bbox_to_anchor=(1.05, 1), 
+        loc='upper left', 
+        borderaxespad=0
+      )
+          
+      # Set labels & title
+      plt.xlabel("Height")
+      plt.ylabel("Weight")
+      plt.title("Data Visualization", fontsize=12, fontweight="bold")
 
     # Show plot in Streamlit
   st.pyplot(fig)
