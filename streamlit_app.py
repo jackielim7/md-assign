d6a14d8ddf879c52b7e483d24d38b2d385bda42e
jackielim7 <jackieops78@gmail.com>
2025-03-24 04:04:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,32 +23,6 @@ def main():
       st.write("This is a raw data")
       st.dataframe(df)
 
-  # with st.expander("**Data Visualization**"):
-  #     # Matplotlib figure
-  #     fig, ax = plt.subplots(figsize=(10  , 5))
-  
-  #     # Create scatter plot
-  #     scatter = sns.scatterplot(
-  #         x=df["Height"], 
-  #         y=df["Weight"], 
-  #         hue=df["NObeyesdad"], 
-  #         palette="Set1", 
-  #         s=50,  # Circle size
-  #         edgecolor=None,  # No border on circles
-  #         ax=ax
-  #     )
-  
-  #     # Set axes to start from (0,0)
-  #     ax.set_xlim(0, 2)
-  #     ax.set_xticks([i / 10 for i in range(0, 21)])
-  #     ax.set_ylim(0, 180)
-  
-  #     # Move legend below the plot with no border
-  #     legend = ax.legend(title="NObeyesdad", bbox_to_anchor=(0.5, -0.1), loc="upper center", frameon=False, ncol=2)
-  
-  #     # Display plot in Streamlit
-  #     st.pyplot(fig)
-
   # Expand for visualization
   with st.expander("**Data Visualization**"):
       # Create an interactive scatter plot
