1c459a81c200650660033af3845f4c1cb350a070
jackielim7 <jackieops78@gmail.com>
2025-03-24 22:44:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,19 +49,19 @@ def main():
       st.plotly_chart(fig)
 
   Gender = st.selectbox('Gender', ('Male', 'Female')) 
-  Age = st.number_input('Age', min_value=1, max_value=70, value=14)  # ✅ Allow typing
+  Age = st.slider('Age', min_value = 1, max_value = 80, value = 1)  # ✅ Allow typing
   Height = st.number_input('Height (meters)', min_value=1.0, max_value=3.0, value=1.0, step=0.01)  # ✅ Allow decimals
   Weight = st.number_input('Weight (kilograms)', min_value=1.0, max_value=200.0, value=1.0, step=0.1)  # ✅ Allow decimals
   family_history_with_overweight = st.selectbox('family_history_with_overweight', ('yes', 'no'))
   FAVC = st.selectbox('FAVC', ('yes', 'no'))
-  FCVC = st.number_input('FCVC', min_value=1, max_value=3, value=1)
-  NCP = st.number_input('NCP', min_value=1, max_value=4, value=1)
+  FCVC = st.number_input('FCVC', min_value=1.0, max_value=3.0, value=1.0, step=0.1) 
+  NCP = st.number_input('NCP', min_value=1.0, max_value=4.0, value=1.0, step=0.1) 
   CAEC = st.selectbox('CAEC', ('Always', 'Frequently', 'Sometimes', 'no'))
   SMOKE = st.selectbox('SMOKE', ('yes', 'no'))
-  CH2O = st.number_input('CH2O', min_value=1, max_value=3, value=1)
+  CH2O = st.number_input('CH2O', min_value=1.0, max_value=3.0, value=1.0, step=0.1) 
   SCC = st.selectbox('SCC', ('yes', 'no'))
-  FAF = st.number_input('FAF', min_value=1, max_value=3, value=1)
-  TUE = st.number_input('TUE', min_value=1, max_value=2, value=1)
+  FAF = st.number_input('FAF', min_value=0.0, max_value=3.0, value=0.0, step=0.1)
+  TUE = st.number_input('TUE', min_value=0.000, max_value=2.000, value=0.000, step=0.001)
   CALC = st.selectbox('CALC', ('Always', 'Frequently', 'Sometimes', 'no'))
   MTRANS = st.selectbox('MTRANS', ('Public_Transportation', 'Automobile', 'Walking', 'Motorbike', 'Bike'))
 
